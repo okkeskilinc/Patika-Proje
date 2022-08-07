d62d336cf880894111a2adaf608679a9da2e0ca9
Okkes Kilinc <45037079+okkeskilinc@users.noreply.github.com>
2022-08-07 17:50:29 +0300

    Update Flatten.py

--- Flatten.py
+++ Flatten.py
@@ -9,6 +9,19 @@ def flatten_list(non_flatten):
     
     #flatten_list.sort()
     return flatten_list
+_list=[]   
+def flatten_for(non_flatten):
+    for i in non_flatten:
+        if type(i)==list:
+            flatten_for(i)
+        else:
+            _list.append(i)
+    return _list
 
 x=[[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+y=[[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+y_f=flatten_for(y)
+y_r=y[::-1]
 print(flatten_list(x))
+print(y_f)
+print(y_r)
