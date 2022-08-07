3855d78407c4d69b879371f3b4f4a12dce801864
Okkes Kilinc <45037079+okkeskilinc@users.noreply.github.com>
2022-08-07 20:49:10 +0300

    Update and rename Flatten.py to Flatten_and_Reverse_List.py

--- Flatten_and_Reverse_List.py
+++ Flatten_and_Reverse_List.py
@@ -1,3 +1,4 @@
+"""
 def flatten_list(non_flatten):
     flatten_list=[]
     while(non_flatten):
@@ -9,7 +10,7 @@ def flatten_list(non_flatten):
     
     #flatten_list.sort()
     return flatten_list
-    
+"""   
 _list=[]   
 def flatten_for(non_flatten):
     for i in non_flatten:
@@ -31,7 +32,7 @@ def reverse_list(n):
 x=[[1,'a',['cat'],2],[[[3]],'dog'],4,5]
 y=[[1, 2], [3, 4], [5, 6, 7]]
 
-y_f=flatten_for(y)
+x_f=flatten_for(x)
 y_r=reverse_list(y)
-print(y_f)
+print(x_f)
 print(y_r)
