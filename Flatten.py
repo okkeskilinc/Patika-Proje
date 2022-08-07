b3ce7bf5543cfbaea77b7b4c978dc5811da75b37
Okkes Kilinc <45037079+okkeskilinc@users.noreply.github.com>
2022-08-07 18:14:45 +0300

    Update Flatten.py

--- Flatten.py
+++ Flatten.py
@@ -9,6 +9,7 @@ def flatten_list(non_flatten):
     
     #flatten_list.sort()
     return flatten_list
+    
 _list=[]   
 def flatten_for(non_flatten):
     for i in non_flatten:
@@ -18,10 +19,19 @@ def flatten_for(non_flatten):
             _list.append(i)
     return _list
 
+_rList=[]
+def reverse_list(n):
+    for i in reversed(n):
+        if type(i)==list:
+            _rList.append(i[::-1])
+        else:
+            _rList.append(i)
+    return _rList
+    
 x=[[1,'a',['cat'],2],[[[3]],'dog'],4,5]
-y=[[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+y=[[1, 2], [3, 4], [5, 6, 7]]
+
 y_f=flatten_for(y)
-y_r=y[::-1]
-print(flatten_list(x))
+y_r=reverse_list(y)
 print(y_f)
 print(y_r)
